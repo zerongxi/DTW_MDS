f84f74c3ae10094cdd590fb8c267ae074ca14fb3
Zerong Xi <xizerong@gmail.com>
2020-10-15 07:41:56 -0400

    initial commit

--- dataloader.py
+++ dataloader.py
@@ -10,7 +10,7 @@ def read_data(path=DATA_PATH):
     :param path:
     :return: controller: a list of 2D array, time_index x (elevator, aileron, throttle)
     task: task id
-    success: success (1.0) or failure (0.0)
+    success: success (True) or failure (False)
     """
     with open(path, "rb") as fp:
         controller, task, success = pickle.load(fp)
