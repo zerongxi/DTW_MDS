c3cfc00aec72d8faf0ba21df15b920209be933c6
Shengnan Hu <sarahhu0109@gmail.com>
2020-10-25 10:23:54 -0400

    test with machine learning classifiers

--- methods.py
+++ methods.py
@@ -14,12 +14,12 @@ def _apply_dtw(x, y, x_idx, y_idx):
 
 def apply_dtw(data):
     args = [(data[i], data[j], i, j) for i, _ in enumerate(data) for j, _ in enumerate(data)]
-    start = time.time()
+    # start = time.time()
     with Pool(N_CPUS) as pool:
         returns = pool.starmap(_apply_dtw, args)
     distance_matrix = np.zeros((len(data), len(data)), dtype=np.float)
     for d, i, j in returns:
         distance_matrix[i, j] = distance_matrix[j, i] = d
-    end = time.time()
-    print (end-start)
+    # end = time.time()
+    # print(end-start)
     return distance_matrix
